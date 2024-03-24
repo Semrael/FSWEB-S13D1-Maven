5363f370facb917136019e27e9b77a999c34a08a
semra-elcelik <semraelcelik@gmail.com>
2024-03-24 16:45:43 +0300

    shouldWakeUp metodu, hasTeen metodu Area metotları tanımlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(shouldWakeUp(true, 1));
@@ -10,7 +12,18 @@ public class Main {
         System.out.println(hasTeen(9, 99, 19));
         System.out.println(hasTeen(23, 15, 42));
         System.out.println(hasTeen(22, 23, 34));
+        System.out.println("----------------------------------------");
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+        System.out.println("--------------------------------------------");
 
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+
+        System.out.println("----------------------------------------");
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
@@ -39,15 +52,33 @@ public class Main {
 
     }
 //
-//    public static boolean isCatPlaying(boolean isSummer, int temp) {
-//
-//    }
-//
-//    public static double area(double width, double height) {
-//
-//    }
-//
-//    public static double area(double radius) {
-//
-//    }
+    public static boolean isCatPlaying(boolean isSummer, int temp) {
+          if(isSummer&&(temp<=45 && temp>=25)){
+              return true;
+          }
+          else if (!isSummer&&(temp<=35 && temp>=25)){
+              return true;
+          }
+          else {
+              return false;
+
+          }
+    }
+    public static double area(double width, double height) {
+
+        if (width<0 || height<0){
+            return -1;
+        }
+        else{
+            return width*height;
+        }
+
+    }
+
+    public static double area(double radius) {
+         if(radius<0){
+             return -1;
+         }
+         else return Math.pow(radius,2)*Math.PI;
+    }
 }
